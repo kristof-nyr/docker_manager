8838fbba579308e5d77c3096ece88f731eb769eb
kristof-nyr <nyari.kristof66@gmail.com>
2022-05-10 11:57:05 +0200

    Init

---
+++ README.md
@@ -0,0 +1,31 @@
+# Docker image kezelő script
+### Készítette: Nyári Kristóf, BFARTM
+---
+## Dokumentáció:
+Ez a script paraméterezettségétől függően eltérő információkat ad vissza docker image-kről
+> A script futtatásához szükséges a `docker`, `curl` és a `jq` package
+
+> Telepítés: OpenSUSE: `zypper install docker` | `zypper install curl` | `zypper install jq`
+
+> RedHat: `rpm -i docker` | `rpm -i curl` | `rpm -i jq`
+
+
+---
+A script indítása előtt a user-t hozzá kell adni a docker csoporthoz
+```bash
+sudo gpasswd -a `(whoami)` docker
+sudo systemctl restart docker
+```
+A script futtatása
+```bash
+chmod u+x ./docker_manager.sh
+
+To list all repos in given namespace
+./docker_manager.sh <namespace>
+
+To list tags of the given repository
+./docker_manager.sh <namespace> <repository>
+
+To pull a certain docker image
+./docker_manager.sh <namespace> <tag>
+```
---
+++ docker_manager.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+#Készítette: Nyári Kristóf
+#Neptun: BFARTM
+
+export readonly DOCKER_USERNAME=bfartmneptun
+export readonly DOCKER_PASSWORD=TY6JFQtFEqiFiz3
+export readonly TOKEN=$(curl -s -H "Content-Type: application/json" -X POST -d '{"username": "'${DOCKER_USERNAME}'", "password": "'${DOCKER_PASSWORD}'"}' https://hub.docker.com/v2/users/login/ | jq -r .token)
+
+#Echo color variables
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+#Ez a metódus ellenőrzi, hogy a script futásához szükséges csomagok telepítve vannak-e
+#Pirossal kiírja ha nem, valamint a telepítés menetét
+#(echo végén a ${NC} visszaváltoztatja a temrinál output színét defaultra)
+checkPackages() {
+    if [[ ! -f /usr/bin/curl ]];then
+        echo -e "\n${RED}curl not found"
+        echo -e "Run: zypper install curl | rpm -i curl${NC}"
+        exit 1
+    fi
+    if [[ ! -f /usr/bin/docker ]];then
+        echo -e "\n${RED}docker not found"
+        echo -e "Run: zypper install docker | rpm -i docker${NC}"
+        exit 1
+    fi
+    if [[ ! -f /usr/bin/jq ]];then
+        echo -e "\n${RED}jq not found"
+        echo -e "Run: zypper install jq | rpm -i jq${NC}"
+        exit 1
+    fi
+}
+
+displayHelp() {
+    echo -e "Wrong number of parameters!\nUsage:";
+    echo -e "\n To list all repos in given namespace:\n ${0##*/} <namespace>"
+    echo -e "\n To list tags of the given repository:\n ${0##*/} <namespace> <repository>"
+    echo -e "\n To pull a certain docker image:\n ${0##*/} <namespace> <repository> <tag>"
+}
+
+getRepositories() {
+    namespace=$1
+    curl -s -H "Authorization: JWT ${TOKEN}" "https://hub.docker.com/v2/repositories/${namespace}/?page_size=25" | jq -r '.results|.[]|.name'
+
+}
+
+getSummary() {
+    namespace=$1
+    repository=$2
+    #curl -s -H "Authorization: JWT ${TOKEN}" "https://hub.docker.com/v2/namespaces/${namespace}/repositories/${repository}/images-summary" | jq '.statistics| {total, active, inactive}'
+    curl -s -H "Authorization: JWT ${TOKEN}" "https://hub.docker.com/v2/repositories/${namespace}/${repository}/tags/?page_size=25" | jq -r '.results|.[]| {name, images}'
+}
+
+pullDockerImage() {
+    namespace=$1
+    repository=$2
+    tag=$3
+    docker pull "${namespace}"/"${repository}":"${tag}"
+}
+
+
+main() {
+    checkPackages
+    case "$#" in
+        1)
+            getRepositories "$1"
+            return
+        ;;
+        2)
+            getSummary "$1" "$2"
+            return
+        ;;
+        3)
+            pullDockerImage "$1" "$2" "$3"
+            return
+        ;;
+        *)
+            displayHelp
+            exit 1;
+        ;;
+    esac
+}
+
+
+main "$@"
